8b7e8053efb18e9e00efd630f2b6a07dca8b6905
Muhammad Mohsin <m.mohsin.zafar@outlook.com>
2023-05-27 10:48:43 +0500

    Initial Commit 1.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardLayoutComponent }, // default route of the module
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NormalLayoutComponent } from './layouts/normal-layout/normal-layout.component';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NavComponent } from './layout-components/nav/nav.component';
+import { FooterComponent } from './layout-components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NormalLayoutComponent,
+    DashboardLayoutComponent,
+    NotFoundComponent,
+    LoginComponent,
+    SignupComponent,
+    NavComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
